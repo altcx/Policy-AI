71cff1d5ca183a88d04e2999e46e39232b58fc61
Andrew Calamug <altcalamug@icloud.com>
2025-06-26 23:14:02 -0400

    Delete types directory

--- types/diff.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'diff' {
-  export function diffLines(oldStr: string, newStr: string): any;
-  export function createTwoFilesPatch(fileName: string, oldFileName: string, oldStr: string, newStr: string): any;
-}
